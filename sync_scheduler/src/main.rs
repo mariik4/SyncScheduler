3afa8d4fb7b0ac8d4a450d40f04322b43a9630b9
PobrodilinS <sempo2003@gmail.com>
2025-04-23 21:53:58 +0200

    Add DB functions. In main added testing of select functions

--- sync_scheduler/src/main.rs
+++ sync_scheduler/src/main.rs
@@ -1,17 +1,21 @@
 include!("./gui_render/calendar_render.rs");
 include!("./gui_render/data_formatter.rs");
 include!("./calendar/calendar_state.rs");
+include!("./database/db_functions.rs");
 
+use chrono::{NaiveDateTime, NaiveTime};
+use slint::{ModelRc, VecModel};
 use std::{
     cell::{RefCell, RefMut},
     rc::Rc,
 };
 
-use slint::{ModelRc, VecModel};
-
 slint::include_modules!();
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
     let calendar_window = CalendarWindow::new().unwrap();
     calendar_window.window().set_maximized(true);
     // Create multi mutable reference to the calendar state
@@ -30,7 +34,7 @@ fn main() {
     let weak_window = calendar_window.as_weak();
     let state_clone = Rc::clone(&calendar_state);
     // Borrow ownership of all variables to ensure their lifecycle, when buttons are clicked
-    // (since it can be out of the scope of the main variable) ass
+    // (since it can be out of the scope of the main variable)
     calendar_window.on_next_month(move || {
         let window = weak_window.unwrap();
         let mut state = state_clone.borrow_mut();
@@ -58,6 +62,17 @@ fn main() {
         state.select_date(&day_id);
         calendar_render(&window, state);
     });
+
+    get_events_in_day(Uuid::nil(), NaiveDate::from_ymd_opt(2025, 4, 18).unwrap())
+        .await
+        .unwrap()
+        .iter()
+        .for_each(|event| {
+            println!(
+                "Event: {} - {} - {}",
+                event.name, event.start_time, event.end_time
+            );
+        });
 
     calendar_window.run().unwrap();
 }
